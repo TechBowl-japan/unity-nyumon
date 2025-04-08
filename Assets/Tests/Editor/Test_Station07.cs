c2284c15b82b73199c7330498e7fad4ccd8f38d2
Keisuke Kamioka <keisukekamioka@Keisukenonotobukkukonpyuta.local>
2025-04-08 20:45:46 +0900

    Fix st7

--- Assets/Tests/Editor/Test_Station07.cs
+++ Assets/Tests/Editor/Test_Station07.cs
@@ -17,7 +17,7 @@ namespace Tests
             var pickUpParentObject = GameObject.Find("PickUp Parent");
             Assert.That(pickUpParentObject, Is.Not.Null, "PickUp Parentが存在しません");
 
-            var pickUpRenderers = pickUpParentObject.transform.GetComponentsInChildren<MeshRenderer>().Where(x => x.CompareTag("PickUp")).ToArray();
+            var pickUpRenderers = pickUpParentObject.transform.GetComponentsInChildren<MeshRenderer>().Where(x => x.name.StartsWith("PickUp")).ToArray();
             Assert.That(pickUpRenderers.Length, Is.EqualTo(4), "PickUp Parentの子にPickUpが4つ存在していません");
 
             var initialRotation = pickUpRenderers[0].transform.rotation;
